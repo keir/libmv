250c645109b4a9aee6f304e4f3e0c7c3e78a8ef6
Keir Mierle <mierle@gmail.com>
2011-06-05 13:36:12 -0700

    Start new region tracker classes.
    The new region tracking classes are intended for tracking between images with
    small baseline movements, such as video. To start this includes a classic KLT
    tracker, and a pyramid tracker which composes with any other tracker. For
    example, to make a pyramid KLT tracker, initialize the PyramidRegionTracker
    with a KltRegionTracker.

---
+++ src/libmv/tracking/region_tracker.h
@@ -0,0 +1,44 @@
+// Copyright (c) 2011 libmv authors.
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to
+// deal in the Software without restriction, including without limitation the
+// rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
+// sell copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+// FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+// IN THE SOFTWARE.
+
+#ifndef LIBMV_TRACKING_TRACKER_H_
+#define LIBMV_TRACKING_TRACKER_H_
+
+#include "libmv/image/image.h"
+
+namespace libmv {
+
+class RegionTracker {
+ public:
+  RegionTracker() {}
+  virtual ~RegionTracker() {}
+
+  // Track a point from image1 to image2. Returns true on success, false
+  // otherwise. x2, y2 should start out as a best guess for the position in
+  // image2; if no guess is available x1, y1 is a good start.
+  virtual bool Track(const FloatImage &image1,
+                     const FloatImage &image2,
+                     double  x1, double  y1,
+                     double *x2, double *y2) const = 0;
+};
+
+}  // namespace libmv
+
+#endif  // LIBMV_CORRESPONDENCE_TRACKER_H_
