cfe8f83cea71d0fe46159cdb4b750c5888ae2065
Matthias Fauconneau <matthias.fauconneau@gmail.com>
2011-06-15 22:49:53 +0200

    NVidia GLSL compiler is really not strict enough...

--- src/ui/tracker/shader.glsl
+++ src/ui/tracker/shader.glsl
@@ -2,7 +2,7 @@ vertex {
   attribute vec4 position;
   image {
     gl_Position = vec4(position.xy,0,1);
-    varying out vec2 texCoord;
+    varying vec2 texCoord;
     texCoord = position.zw;
   }
   transform {
@@ -16,22 +16,21 @@ vertex {
 }
 
 fragment {
-  varying out vec4 color;
   image {
-    varying in vec2 texCoord;
+    attribute vec2 texCoord;
     uniform sampler2D image;
-    color = texture2D(image,texCoord);
+    gl_FragColor = texture2D(image,texCoord);
   }
   marker {
-    color = vec4(0,0.25,0,1);
+    gl_FragColor = vec4(0,0.25,0,1);
   }
   bundle {
-    color = vec4(0,0.25,0,1) * (1-length(2*gl_PointCoord.xy-vec2(1,1)));
+    gl_FragColor = vec4(0,0.25,0,1) * (1-length(2*gl_PointCoord.xy-vec2(1,1)));
   }
   camera {
-    color = vec4(0.25,0,0,1);
+    gl_FragColor = vec4(0.25,0,0,1);
   }
   object {
-    color = vec4(0,0,0.25,1);
+    gl_FragColor = vec4(0,0,0.25,1);
   }
 }
